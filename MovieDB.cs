32719bb923868e510d0e68182b2fef56f7654a4a
Mads Jakob Steffansen <mads.steffansen@cb.dk>
2018-08-10 15:59:57 +0200

    mdb

---
+++ MovieDB.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Data.SqlClient;
+
+namespace X
+{
+    public class MyClass 
+    {
+        public  static void Test()
+        {
+            SqlConnection x = new SqlConnection("");
+        }
+    }
+
+}
